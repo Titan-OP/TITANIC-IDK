7cd178f91c5fc0e768b956537f9ec77446d750d0
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-18 12:02:01 +0530

    Update __shield.py

--- YoneRobot/modules/__shield.py
+++ YoneRobot/modules/__shield.py
@@ -430,7 +430,7 @@ async def del_profanity(event):
 
 __help__ = """
 <b> Group Guardian: </b>
-✪ Yone can protect your group from NSFW senders, Slag word users and also can force members to use English
+Nezuko can protect your group from NSFW senders, Slag word users and also can force members to use English
 
 <b>Commmands</b>
  - /gshield <i>on/off</i> - Enable|Disable Porn cleaning
