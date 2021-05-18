8da6f2188254671996208bb27ab94c9d78087c78
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-18 18:47:18 +0530

    Update ___aifunctions.py

--- YoneRobot/modules/___aifunctions.py
+++ YoneRobot/modules/___aifunctions.py
@@ -3,8 +3,8 @@
 
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from AsunaRobot import telethn, OWNER_ID
-from AsunaRobot.events import register
+from YoneRobot import telethn, OWNER_ID
+from YoneRobot.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
