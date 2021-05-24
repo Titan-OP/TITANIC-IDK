6f7a6a549d138039f21ea86b6cc917362554d85e
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-24 11:07:32 +0530

    Update about.py

--- YoneRobot/modules/about.py
+++ YoneRobot/modules/about.py
@@ -4,8 +4,5 @@ from YoneRobot.events import register
 from telegram import Update
 
 @register(pattern=("/about"))
-async def kaneki(update: Update):
-          msg = update.effective_message
-          Message = msg.reply_text("heyo there")
-
-          Message.edit_text("hey there")
+async def kaneki(event):
+          await event.reply("hi")
