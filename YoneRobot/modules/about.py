3ba2feb7ca1459b98b5570e8a8e98cee1c6edcb1
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-28 14:32:49 +0530

    Update about.py

--- YoneRobot/modules/about.py
+++ YoneRobot/modules/about.py
@@ -5,11 +5,11 @@ from telegram import Update
 
 @register(pattern=("/about"))
 async def kaneki(event):
-          await event.reply("Heyo, im *nezuko* from kimestu no yaiba.
-                            \nA powerful bot to help you manage your group.
+          await event.reply("""Heyo, im *nezuko* from kimestu no yaiba.
+                             \nA powerful bot to help you manage your group.
                              \nHit /help to know my features.
                              \nI can restrict users.
                              \nI can warn users when they reach max warns I will ban or kick them.
                              \nI can keep filters and notes and many more to know all of them hit /help.
                              \nAny questions regarding me head to [support group](t.me/nezukosupport1)
-                             \nKeep updated about nezuko by joining [updates](t.me/nezukoupdates1) channel")
+                             \nKeep updated about nezuko by joining [updates](t.me/nezukoupdates1) channel""")
