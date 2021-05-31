0d81a7484fc1e1273bfc3a1b2f2802f76d96301d
kanekiken44 <81549240+kanekiken44@users.noreply.github.com>
2021-05-31 10:57:50 +0530

    Update purge.py

--- YoneRobot/modules/purge.py
+++ YoneRobot/modules/purge.py
@@ -45,8 +45,10 @@ async def purge_messages(event):
         pass
     time_ = time.perf_counter() - start
     text = f"Purged Successfully"
-    await event.respond(text, del_in=30.0)
-        
+    await event.respond(text)
+    m = (text) 
+    await asyncio.sleep(20)
+    await m.del()
 
 async def delete_messages(event):
     if event.from_id is None:
