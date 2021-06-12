70d1a215bc522a394ce06199516844d90ce02b67
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-12 19:57:39 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -547,7 +547,7 @@ def settings_button(update: Update, context: CallbackContext):
             chat_id = back_match.group(1)
             chat = bot.get_chat(chat_id)
             query.message.reply_text(
-                text=""нι тнєяє! тнєяє αяє qυιтє α ƒєω ѕєттιηgѕ ƒσя {} - gσ αнєα∂ αη∂ ριcк ωнαт "
+                text="нι тнєяє! тнєяє αяє qυιтє α ƒєω ѕєттιηgѕ ƒσя {} - gσ αнєα∂ αη∂ ριcк ωнαт "
                 "уσυ'яє ιηтєяєѕтє∂ ιη."".format(escape_markdown(chat.title)),
                 parse_mode=ParseMode.MARKDOWN,
                 reply_markup=InlineKeyboardMarkup(
