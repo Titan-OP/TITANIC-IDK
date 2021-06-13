62b798b6f18de5bea8762e6ff555ab460793b7fd
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-13 18:38:33 +0530

    Update errors.py

--- YoneRobot/utils/errors.py
+++ YoneRobot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 from YoneRobot import pbot as app
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-LOG_GROUP_ID = int(-1001196083551)
+LOG_GROUP_ID = int(-1001327535845)
 
 def split_limits(text):
     if len(text) < 2048:
