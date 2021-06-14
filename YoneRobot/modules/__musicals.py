620ef283fbdfe918fd06e067b2adbd6e82c7de33
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-14 16:50:02 +0530

    Update __musicals.py

--- YoneRobot/modules/__musicals.py
+++ YoneRobot/modules/__musicals.py
@@ -1,9 +1,15 @@
 __mod_name__ = "Mᴜꜱɪᴄ🎶"
 
 __help__ = """
-/video <i>query</i>: ∂σωηℓσα∂ νι∂єσ ƒяσм уσυтυвє
-/deezer <i>query</i>: ∂σωηℓσα∂ ƒяσм ∂єєzєя
-/music <i>query</i>: ∂σωηℓσα∂ ѕσηg ƒяσм ʏᴛ ѕєяνєяѕ. `(API BASED)`
-/lyrics <i>song name</i> : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє.
-/glyrics <i> song name </i> : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє αη∂ αятιѕт.
+
+ _
+- /video <i>query</i>: ∂σωηℓσα∂ νι∂єσ ƒяσм уσυтυвє
+
+- /deezer <i>query</i>: ∂σωηℓσα∂ ƒяσм ∂єєzєя
+
+- /music <i>query</i>: ∂σωηℓσα∂ ѕσηg ƒяσм ʏᴛ ѕєяνєяѕ. `(API BASED)`
+
+- /lyrics <i>song name</i> : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє.
+
+- /glyrics <i> song name </i> : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє αη∂ αятιѕт.
 """
