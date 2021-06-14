e4090e506fd5597bddc0930a8b6c54a94d7896f1
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-14 15:33:34 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -221,7 +221,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "ι'м αωαкє αℓяєα∂у!\n<b>нανєη'т ѕℓєρт ѕιηcє:</b> <code>{}</code>".format(
+            "✨ι'м αℓяєα∂у αωαкє!✨\n<b>нανєη'т ѕℓєρт ѕιηcє:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
