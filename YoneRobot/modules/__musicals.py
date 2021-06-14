a32642d3b04c4896d882407ba75a8a6a2606962e
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-14 16:59:17 +0530

    Update __musicals.py

--- YoneRobot/modules/__musicals.py
+++ YoneRobot/modules/__musicals.py
@@ -1,13 +1,13 @@
 __mod_name__ = "Mᴜꜱɪᴄ🎶"
 
 __help__ = """
-- /video query : ∂σωηℓσα∂ νι∂єσ ƒяσм уσυтυвє
+✮ /video query : ∂σωηℓσα∂ νι∂єσ ƒяσм уσυтυвє
 
-- /deezer query : ∂σωηℓσα∂ ƒяσм ∂єєzєя
+✮ /deezer query : ∂σωηℓσα∂ ƒяσм ∂єєzєя
 
-- /music query : ∂σωηℓσα∂ ѕσηg ƒяσм ʏᴛ ѕєяνєяѕ. `(API BASED)`
+✮ /music query : ∂σωηℓσα∂ ѕσηg ƒяσм ʏᴛ ѕєяνєяѕ. `(API BASED)`
 
-- /lyrics song name : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє.
+✮ /lyrics song name : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє.
 
-- /glyrics song name : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє αη∂ αятιѕт.
+✮ /glyrics song name : тнιѕ ρℓυgιη ѕєαяcнєѕ ƒσя ѕσηg ℓуяιcѕ ωιтн ѕσηg ηαмє αη∂ αятιѕт.
 """
