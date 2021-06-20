d7ab118a4107797508b6b4d8bdd6584b49ad9c4f
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-20 19:51:20 +0530

    Update afk.py

--- YoneRobot/modules/afk.py
+++ YoneRobot/modules/afk.py
@@ -137,14 +137,12 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             return
         if not user.reason:
             res = "{} is afk".format(fst_name)
-            update.effective_message.reply_animation(AFK_REASON_VID, caption="{} is afk.\nReason: <code>{}</code>".format(
-                html.escape(fst_name), html.escape(user.reason))
+            update.effective_message.reply_animation(AFK_REASON_VID, caption="{} is afk.\nReason: <code>{}</code>".format(html.escape(fst_name), html.escape(user.reason))
         else:
             res = "{} is afk.\nReason: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
-            update.effective_message.reply_animation(AFK_REASON_VID, caption="{} is afk.\nReason: <code>{}</code>".format(
-                html.escape(fst_name), html.escape(user.reason))
+            update.effective_message.reply_animation(AFK_REASON_VID, caption="{} is afk.\nReason: <code>{}</code>".format(html.escape(fst_name), html.escape(user.reason))
 
 
 
