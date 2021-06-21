14dfb79bd8a6065e6bd814c538697d1adf4f2eb9
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-21 10:57:19 +0530

    Create cleaning.py

--- YoneRobot/modules/cleaning.py
+++ YoneRobot/modules/cleaning.py
@@ -411,26 +411,6 @@ __help__ = """
 
  ✮ /listblue*:* list currently whitelisted commands
 
-*Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
-will result in restricting that user.
- This will mute users if they send more than 10 messages in a row, bots are ignored.
- ✮ /flood*:* Get the current flood control setting
-
-• *Admins only:*
- ✮ /setflood <int/'no'/'off'>*:* enables or disables flood control
- *Example:* `/setflood 10`
-
- ✮ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
-
-• *Note:*
- • Value must be filled for tban and tmute!!
- It can be:
- `5m` = 5 minutes
- `6h` = 6 hours
- `3d` = 3 days
- `1w` = 1 week
- """
-
 __mod_name__ = "Cᴏɴᴛʀᴏʟꜱ🛂"
 
 FLOOD_BAN_HANDLER = MessageHandler(
