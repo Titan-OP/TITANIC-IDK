976c9fee1fa9bdae86132aed3de4058e97e3d11a
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-21 10:49:49 +0530

    Update logomaker.py

--- YoneRobot/modules/logomaker.py
+++ YoneRobot/modules/logomaker.py
@@ -5,7 +5,7 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 
-@tbot.on(events.NewMessage(pattern="^/logo ?(.*)"))
+@register(pattern="^/logo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
@@ -45,7 +45,7 @@ async def lego(event):
 
 
    
-@tbot.on(events.NewMessage(pattern="^/ptxt ?(.*)"))
+@register(pattern="^/ptxt ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
