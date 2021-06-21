029d06babe83c6617e0d4a41ecae7ae84e0532a4
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-21 10:56:29 +0530

    Create antiflood.py

---
+++ YoneRobot/modules/antiflood.py
@@ -0,0 +1,44 @@
+*Antiflood* allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
+will result in restricting that user.
+ This will mute users if they send more than 10 messages in a row, bots are ignored.
+ ✮ /flood*:* Get the current flood control setting
+
+• *Admins only:*
+ ✮ /setflood <int/'no'/'off'>*:* enables or disables flood control
+ *Example:* `/setflood 10`
+
+ ✮ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+
+• *Note:*
+ • Value must be filled for tban and tmute!!
+ It can be:
+ `5m` = 5 minutes
+ `6h` = 6 hours
+ `3d` = 3 days
+ `1w` = 1 week
+ """
+
+__mod_name__ = "Aɴᴛɪꜰʟᴏᴏᴅ⚔️"
+
+FLOOD_BAN_HANDLER = MessageHandler(
+    Filters.all & ~Filters.status_update & Filters.group, check_flood
+)
+SET_FLOOD_HANDLER = CommandHandler("setflood", set_flood, filters=Filters.group)
+SET_FLOOD_MODE_HANDLER = CommandHandler(
+    "setfloodmode", set_flood_mode, pass_args=True
+)  # , filters=Filters.group)
+FLOOD_QUERY_HANDLER = CallbackQueryHandler(flood_button, pattern=r"unmute_flooder")
+FLOOD_HANDLER = CommandHandler("flood", flood, filters=Filters.group)
+
+dispatcher.add_handler(FLOOD_BAN_HANDLER, FLOOD_GROUP)
+dispatcher.add_handler(FLOOD_QUERY_HANDLER)
+dispatcher.add_handler(SET_FLOOD_HANDLER)
+dispatcher.add_handler(SET_FLOOD_MODE_HANDLER)
+dispatcher.add_handler(FLOOD_HANDLER)
+
+__handlers__ = [
+    (FLOOD_BAN_HANDLER, FLOOD_GROUP),
+    SET_FLOOD_HANDLER,
+    FLOOD_HANDLER,
+    SET_FLOOD_MODE_HANDLER,
+]
