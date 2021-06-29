4a0dd88a0ce53bc30a5e73a42f57f962d2383cea
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-29 14:31:27 +0530

    Update disable.py

--- YoneRobot/modules/disable.py
+++ YoneRobot/modules/disable.py
@@ -341,18 +341,18 @@ if is_module_loaded(FILENAME):
     ✮ /dcmds *:* check the current status of disabled commands
 
     *Admins only:*
-  ✮ /enable <cmd name> *:* enable that command
+    ✮ /enable <cmd name> *:* enable that command
 
-  ✮ /disable <cmd name> *:* disable that command
+    ✮ /disable <cmd name> *:* disable that command
+ 
+    ✮ /enablemodule <module name> *:* enable all commands in that module
 
-  ✮ /enablemodule <module name> *:* enable all commands in that module
+    ✮ /disablemodule <module name> *:* disable all commands in that module
 
-  ✮ /disablemodule <module name> *:* disable all commands in that module
-
-  ✮ /listcmds*:* list all possible toggleable commands
+    ✮ /listcmds*:* list all possible toggleable commands
     """
 
-__mod_name__ = "Dɪꜱᴀʙʟᴇ⛔"
+  __mod_name__ = "Dɪꜱᴀʙʟᴇ⛔"
 
 else:
     DisableAbleCommandHandler = CommandHandler
