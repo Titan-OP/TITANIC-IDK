0e49441b3db3d80619f1c6288efe96a6928af759
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-29 17:11:49 +0530

    Update disasters.py

--- YoneRobot/modules/disasters.py
+++ YoneRobot/modules/disasters.py
@@ -676,7 +676,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Dᴇᴠ Cᴏᴍᴍᴀɴᴅꜱ👑"
+__mod_name__ = "Dᴇᴠ Cᴍᴅꜱ👑"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
