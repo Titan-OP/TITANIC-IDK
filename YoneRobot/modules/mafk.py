2d701033d81e8185d824a90dd5f66c083f58946b
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-06-30 12:22:43 +0530

    Update mafk.py

--- YoneRobot/modules/mafk.py
+++ YoneRobot/modules/mafk.py
@@ -127,7 +127,7 @@ def reply_afk(update: Update, context: CallbackContext):
     elif message.reply_to_message:
         user_id = message.reply_to_message.from_user.id
         fst_name = message.reply_to_message.from_user.first_name
-        check_mafk(update, context, user_id, fst_name, userc_id)
+        check_afk(update, context, user_id, fst_name, userc_id)
 
 
 def check_afk(update, context, user_id, fst_name, userc_id):
