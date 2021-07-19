ed303d607a4f762f567459579fd05013232c318a
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-07-19 13:34:03 +0530

    Update events.py

--- YoneRobot/events.py
+++ YoneRobot/events.py
@@ -18,14 +18,14 @@ gbanned = db.gban
 
 def register(**args):
     """ Registers a new message. """
-    pattern = args.get("pattern", None)
+    pattern = args.get('pattern', None)
 
-    r_pattern = r"^[/!]"
+    r_pattern = r'^[/!]'
 
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
 
-    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
 
     def decorator(func):
         telethn.add_event_handler(func, events.NewMessage(**args))
@@ -56,10 +56,10 @@ def userupdate(**args):
 
 def inlinequery(**args):
     """ Registers inline query. """
-    pattern = args.get("pattern", None)
+    pattern = args.get('pattern', None)
 
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
 
     def decorator(func):
         telethn.add_event_handler(func, events.InlineQuery(**args))
