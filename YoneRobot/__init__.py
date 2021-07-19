854ded4d8066a8c306aa416b35116ef737cb2dd5
Techno Pro <83203983+Titan-OP@users.noreply.github.com>
2021-07-19 13:23:46 +0530

    Update __init__.py

--- YoneRobot/__init__.py
+++ YoneRobot/__init__.py
@@ -96,6 +96,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get("SPAMWATCH_SUPPORT_CHAT", None)
     SPAMWATCH_API = os.environ.get("SPAMWATCH_API", None)
+    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", None)
 
     ALLOW_CHATS = os.environ.get("ALLOW_CHATS", True)
 
